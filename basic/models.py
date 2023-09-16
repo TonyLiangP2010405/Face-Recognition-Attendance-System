609b897d4b2dcc6c5c22e4b670ce7a8bafb3c8e1
Tony Liang <P2010405@mpu.edu.mo>
2023-09-16 16:52:37 +0800

    create the database of system

---
+++ basic/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+from users.models import Student, Course
+
+
+# Create your models here.
+class AttendanceReport(models.Model):
+    AttendanceID = models.IntegerField(primary_key=True)
+    status_choice =(
+        (0, "0"),
+        (1, "1"),
+    )
+    Present_status = models.CharField(max_length=1, choices=status_choice)
+    Absent_rate = models.FloatField()
+    CourseID = models.ForeignKey(Course, on_delete=models.CASCADE)
+    Attendance_start_time = models.DateTimeField()
+
+    def __str__(self):
+        return str(self.AttendanceID) + str(self.Present_status) + str(self.Absent_rate)
+
+    class Meta:
+        verbose_name = "Attendance"
+        verbose_name_plural = "Attendance"
+        db_table = "d_attendance"
+
+
+class StudentAttendance(models.Model):
+    Sta_ID = models.IntegerField(primary_key=True)
+    StudentID = models.ForeignKey(Student, on_delete=models.CASCADE)
+    AttendanceID = models.ForeignKey(AttendanceReport, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.Sta_ID) + str(self.StudentID) + str(self.AttendanceID)
+
+    class Meta:
+        verbose_name = "student attendance"
+        verbose_name_plural = "student attendance"
+        db_table = "d_studentAttendance"
+
---
+++ users/models.py
@@ -0,0 +1,84 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Teacher(models.Model):
+    TeacherID = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=20)
+    teac_email = models.CharField(max_length=100)
+    gender_choice = (
+        (0, "0"),
+        (1, "1"),
+    )
+    gender = models.CharField(max_length=1, choices=gender_choice)
+    age = models.IntegerField
+    teac_image = models.ImageField(upload_to="teacherImage", blank=True)
+
+    def __str__(self):
+        return str(self.TeacherID) + str(self.name) + str(self.gender)
+
+    class Meta:
+        verbose_name = "teacher information"
+        verbose_name_plural = "teacher_information"
+        db_table = 'd_teacher'
+
+
+class Course(models.Model):
+    CourseID = models.CharField(max_length=20)
+    course_name = models.CharField(max_length=100)
+    TeacherID = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    StartTime = models.TimeField()
+    EndTime = models.TimeField()
+    ClassID = models.CharField(max_length=20)
+    classroom_no = models.CharField(max_length=10)
+    course_date = models.DateField()
+    course_choice = (
+        (0, "0"),
+        (1, "1"),
+    )
+    course_state = models.CharField(max_length=1, choices=course_choice)
+
+    def __str__(self):
+        return str(self.CourseID) + str(self.course_name)
+
+    class Meta:
+        verbose_name = "Course information"
+        verbose_name_plural = "course_information"
+        db_table = "d_course"
+
+
+class Student(models.Model):
+    StudentID = models.CharField(max_length=8)
+    name = models.CharField(max_length=20)
+    address = models.TextField()
+    stud_email = models.CharField(max_length=100)
+    gender_choice = (
+        (0, "0"),
+        (1, "1"),
+    )
+    gender = models.CharField(max_length=1, choices=gender_choice)
+    age = models.IntegerField()
+    stud_image = models.ImageField(upload_to="studentImage", blank=True)
+
+    def __str__(self):
+        return str(self.StudentID) + str(self.name) + str(self.gender)
+
+    class Meta:
+        verbose_name = "student information"
+        verbose_name_plural = "student_information"
+        db_table = "d_student"
+
+
+class Enrollment(models.Model):
+    ID = models.IntegerField(primary_key=True)
+    StudentID = models.ForeignKey(Student, on_delete=models.CASCADE)
+    CourseID = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.ID) + str(self.StudentID) + str(self.CourseID)
+
+    class Meta:
+        verbose_name = "Enrollment"
+        verbose_name_plural = "Enrollment"
+        db_table = "d_enrollment"
